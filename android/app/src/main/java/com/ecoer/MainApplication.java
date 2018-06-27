e197ecc6ef1b4d0b719ceba6b7f951f305329157
ZhouTing <zhouting@inhand.com.cn>
2018-06-27 23:41:39 +0800

    加入react-native-image-resizer

--- android/app/src/main/java/com/ecoer/MainApplication.java
+++ android/app/src/main/java/com/ecoer/MainApplication.java
@@ -3,6 +3,7 @@ package com.ecoer;
 import android.app.Application;
 import com.reactnativecomponent.barcode.RCTCapturePackage;
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImageResizerPackage(),
             new PickerPackage(),
           new RNFetchBlobPackage(),
           new RNI18nPackage(),
